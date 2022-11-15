543f41e9deb8e97f8328d0f2998cac4707670082
Shuhao Zhang <szhang590@bloomberg.net>
2022-11-15 00:32:38 +0000

    fix visual

--- lib/visual.ml
+++ lib/visual.ml
@@ -13,44 +13,60 @@ let get_node (type a) (show : a -> string) (exp : a expr) label =
 
 let float_show = Printf.sprintf "%0.2f"
 
+type label_rs = int -> int * node any
+
+let label_ternary : (unit, label_rs) ternary = failwith "TODO"
+
 (* pre order traversal using label *)
-let label_binary (exp : 'a expr) fl fr label =
-  let left_label, left = fl (label + 1) in
-  let right_label, right = fr (left_label + 1) in
-  let result =
-    match exp with
-    | Add _ -> add_tag (get_node float_show exp label) left right
-    | Sub _ -> sub_tag (get_node float_show exp label) left right
-    | Div _ -> div_tag (get_node float_show exp label) left right
-    | Mul _ -> mul_tag (get_node float_show exp label) left right
-    | _ -> failwith binary_warning
+let label_binary =
+  let bop : type a. (_, a) tag_expr -> label_rs -> label_rs -> label_rs =
+   fun exp fl fr label ->
+    let left_label, left = fl (label + 1) in
+    let right_label, right = fr (left_label + 1) in
+    let result =
+      match exp with
+      | Add _ -> add_any_tag (get_node float_show exp label) left right
+      | Sub _ -> sub_any_tag (get_node float_show exp label) left right
+      | Div _ -> div_any_tag (get_node float_show exp label) left right
+      | Mul _ -> mul_any_tag (get_node float_show exp label) left right
+      | _ -> failwith binary_warning
+    in
+    (right_label, result)
   in
-  (right_label, result)
+  { bop }
 
-let label_unary (exp : 'a expr) f label =
-  let final_label, value = f (label + 1) in
-  let result =
-    match exp with
-    | Sin _ -> sin_tag (get_node float_show exp label) value
-    | Cos _ -> cos_tag (get_node float_show exp label) value
-    | Ln _ -> ln_tag (get_node float_show exp label) value
-    | E _ -> e_tag (get_node float_show exp label) value
-    | Sqrt _ -> sqrt_tag (get_node float_show exp label) value
-    | _ -> failwith unary_warning
+let label_unary =
+  let uop : type a. (_, a) tag_expr -> label_rs -> label_rs =
+   fun exp f label ->
+    let final_label, value = f (label + 1) in
+    let result =
+      match exp with
+      | Sin _ -> sin_any_tag (get_node float_show exp label) value
+      | Cos _ -> cos_any_tag (get_node float_show exp label) value
+      | Ln _ -> ln_any_tag (get_node float_show exp label) value
+      | E _ -> e_any_tag (get_node float_show exp label) value
+      | Sqrt _ -> sqrt_any_tag (get_node float_show exp label) value
+      | _ -> failwith unary_warning
+    in
+    (final_label, result)
   in
-  (final_label, result)
+  { uop }
 
-let label_nullary (exp : 'a expr) label =
-  let node = get_node float_show exp label in
-  match exp with
-  | Zero _ -> (label, zero_tag node)
-  | One _ -> (label, one_tag node)
-  | Const (_, a) -> (label, const_tag node a)
-  | Var (_, id) -> (label, var_tag node id)
-  | _ -> failwith nullary_warning
+let label_nullary =
+  let nop : type a. (_, a) tag_expr -> label_rs =
+   fun exp label ->
+    match exp with
+    | Zero _ -> (label, zero_any_tag (get_node float_show exp label))
+    | One _ -> (label, one_any_tag (get_node float_show exp label))
+    | Const (_, a) -> (label, const_any_tag (get_node float_show exp label) a)
+    | Var (_, id) -> (label, var_any_tag (get_node float_show exp label) id)
+    | _ -> failwith nullary_warning
+  in
+  { nop }
 
 let label x =
-  fold_cps label_binary label_unary label_nullary x (fun f -> f 1 |> snd)
+  fold_cps label_ternary label_binary label_unary label_nullary x (fun f ->
+      f 1 |> snd)
 
 type stmt = Edge of int * int | Node of int * string
 
@@ -65,46 +81,61 @@ let stmt_of_node ?(name_env = empty) = function
       | None -> Node (id, Printf.sprintf "x%d" (-1 * id)))
   | Op (id, label) -> Node (id, label)
 
-let dot_binary exp a b =
-  let n = get_tag exp in
-  let node_id = id_of_node n in
-  let gen_stmts left right =
-    let l_node = get_tag left |> id_of_node in
-    let r_node = get_tag right |> id_of_node in
-    [ Edge (l_node, node_id); Edge (r_node, node_id); stmt_of_node n ]
-  in
-  match exp with
-  | Add (_, left, right) -> gen_stmts left right @ a @ b
-  | Sub (_, left, right) -> gen_stmts left right @ a @ b
-  | Div (_, left, right) -> gen_stmts left right @ a @ b
-  | Mul (_, left, right) -> gen_stmts left right @ a @ b
-  | _ -> failwith binary_warning
+let dot_ternary : (node, stmt list) ternary = failwith "TODO"
 
-let dot_unary exp a =
-  let n = get_tag exp in
-  let node_id = id_of_node n in
-  let gen_stmts sub_exp =
-    let sub_node = get_tag sub_exp |> id_of_node in
-    [ Edge (sub_node, node_id); stmt_of_node n ]
+let dot_binary =
+  let bop : type a. (node, a) tag_expr -> stmt list -> stmt list -> stmt list =
+   fun exp a b ->
+    let n = get_tag exp in
+    let node_id = id_of_node n in
+    let gen_stmts left right =
+      let l_node = get_tag left |> id_of_node in
+      let r_node = get_tag right |> id_of_node in
+      [ Edge (l_node, node_id); Edge (r_node, node_id); stmt_of_node n ]
+    in
+    match exp with
+    | Add (_, left, right) -> gen_stmts left right @ a @ b
+    | Sub (_, left, right) -> gen_stmts left right @ a @ b
+    | Div (_, left, right) -> gen_stmts left right @ a @ b
+    | Mul (_, left, right) -> gen_stmts left right @ a @ b
+    | _ -> failwith binary_warning
   in
-  match exp with
-  | Sin (_, sub_exp) -> gen_stmts sub_exp @ a
-  | Cos (_, sub_exp) -> gen_stmts sub_exp @ a
-  | Ln (_, sub_exp) -> gen_stmts sub_exp @ a
-  | E (_, sub_exp) -> gen_stmts sub_exp @ a
-  | Sqrt (_, sub_exp) -> gen_stmts sub_exp @ a
-  | _ -> failwith unary_warning
+  { bop }
 
-let dot_nullary (name_env : string env) exp =
-  match exp with
-  | Zero tag -> [ stmt_of_node tag ]
-  | One tag -> [ stmt_of_node tag ]
-  | Const (tag, _) -> [ stmt_of_node tag ]
-  | Var (tag, _) -> [ stmt_of_node ~name_env tag ]
-  | _ -> failwith nullary_warning
+let dot_unary =
+  let uop : type a. (node, a) tag_expr -> stmt list -> stmt list =
+   fun exp a ->
+    let n = get_tag exp in
+    let node_id = id_of_node n in
+    let gen_stmts sub_exp =
+      let sub_node = get_tag sub_exp |> id_of_node in
+      [ Edge (sub_node, node_id); stmt_of_node n ]
+    in
+    match exp with
+    | Sin (_, sub_exp) -> gen_stmts sub_exp @ a
+    | Cos (_, sub_exp) -> gen_stmts sub_exp @ a
+    | Ln (_, sub_exp) -> gen_stmts sub_exp @ a
+    | E (_, sub_exp) -> gen_stmts sub_exp @ a
+    | Sqrt (_, sub_exp) -> gen_stmts sub_exp @ a
+    | _ -> failwith unary_warning
+  in
+  { uop }
+
+let dot_nullary (name_env : string env) =
+  let nop : type a. (node, a) tag_expr -> stmt list =
+   fun exp ->
+    match exp with
+    | Zero tag -> [ stmt_of_node tag ]
+    | One tag -> [ stmt_of_node tag ]
+    | Const (tag, _) -> [ stmt_of_node tag ]
+    | Var (tag, _) -> [ stmt_of_node ~name_env tag ]
+    | _ -> failwith nullary_warning
+  in
+  { nop }
 
 let stmts_of_label_expr ?(name_env = empty) exp =
-  fold_cps dot_binary dot_unary (dot_nullary name_env) exp Base.Fn.id
+  fold_cps dot_ternary dot_binary dot_unary (dot_nullary name_env) exp
+    Base.Fn.id
 
 let graph_of_stmts stmts =
   let stmts = List.map string_of_stmt stmts in
@@ -112,4 +143,10 @@ let graph_of_stmts stmts =
   Printf.sprintf "digraph G {\n%s\n}\n" graph
 
 let graph ?name_env exp =
-  exp |> label |> stmts_of_label_expr ?name_env |> graph_of_stmts
+  let labelled_exp = label exp in
+  spread labelled_exp
+    {
+      run =
+        (fun tag_expr ->
+          tag_expr |> stmts_of_label_expr ?name_env |> graph_of_stmts);
+    }
